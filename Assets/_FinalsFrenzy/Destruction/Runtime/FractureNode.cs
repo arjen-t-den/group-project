4227ed457072acdcdc2fb0d044f129f3237e37a4
KadenZombie8 <kadenzombie8@gmail.com>
2026-04-09 22:19:01 +0100

    Improved table prop
    Now has another layer of destruction.

---
+++ Assets/_FinalsFrenzy/Destruction/Runtime/FractureNode.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Group8.FinalsFrenzy.Destruction
+{
+    public class FractureNode : MonoBehaviour
+    {
+        public List<FractureNode> _children;
+    }
+}
---
+++ Assets/_FinalsFrenzy/Destruction/Runtime/Part.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace Group8.FinalsFrenzy.Destruction
+{
+    public class Part : MonoBehaviour
+    {
+        [SerializeField]
+        private Part[] _connectedParts;
+    }
+}
